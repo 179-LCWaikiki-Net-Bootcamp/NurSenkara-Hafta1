ead99462475b3bede90a2aa1272ade3ffc54c1de
Nur Şenkara <nsenkara19@gmail.com>
2022-04-14 15:13:20 +0300

    1.hafta ödevi yüklendi.

---
+++ BookStore/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        [Range(1, 10)]
+        public int GenreId { get; set; }
+        public int PageCount { get; set; }
+        public DateTime PublishedDate { get; set; }
+    }
+}
